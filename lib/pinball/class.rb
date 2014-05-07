0a9433e6497d5337cf7200ff141052f71d12d043
Gleb Sinyavsky <zhulik.gleb@gmail.com>
2014-05-07 12:55:02 +0400

    class_inject

--- lib/pinball/class.rb
+++ lib/pinball/class.rb
@@ -8,6 +8,14 @@ class Class
     end
   end
 
+  def class_inject(*deps)
+    deps.each do |dep|
+      define_singleton_method dep do
+        Pinball::Container.instance.items[dep].fetch(self)
+      end
+    end
+  end
+
   def check_pinball
     unless self.is_a? Pinball
       self.extend Pinball
